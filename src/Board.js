4e2b14d1cab190df587834f706be5d86a5fb75fd
AlexSim0219 <alexandersimmons19910219@gmail.com>
2023-11-07 13:26:06 -0800

    first commit

---
+++ src/Board.js
@@ -0,0 +1,30 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function Square({value}) {
+    return <button className='square'>{value}</button>
+}
+
+function Board() {
+  return(
+    <>
+      <div className='board-row'>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className='board-row'>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className='board-row'>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  )
+}
+
+export default Board;
